608b40814363da9bcb7ddd5d9ebdc0bef35cf0b8
John Galt <ekr990011@gmail.com>
2016-08-01 20:14:12 +0000

    adminrole

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+admin = User.create(username: 'admin', email: 'admin@email.com', password: 'password', 
+                    password_confirmation: 'password', role: 'admin')
