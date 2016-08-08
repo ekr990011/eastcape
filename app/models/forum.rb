16e4f7cc9473e2499844e4546d8017b0e31c9740
John Galt <ekr990011@gmail.com>
2016-08-08 20:23:27 +0000

    DESC on both comments and forums

--- app/models/forum.rb
+++ app/models/forum.rb
@@ -1,6 +1,7 @@
 class Forum < ActiveRecord::Base
   belongs_to :user
   has_many :comments, dependent: :destroy
+  default_scope -> { order(created_at: :desc) }
   
   validates :title, presence: true,
                     length: { minimum: 1, maximum: 50 }
