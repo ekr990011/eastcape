6eefd6c07f4b1cca570e03a78f388481732b6055
John Galt <ekr990011@gmail.com>
2016-08-12 17:37:14 +0000

    Comments on Post layout. Comments edit layout. Homepage layout.

--- app/helpers/forums_helper.rb
+++ app/helpers/forums_helper.rb
@@ -1,2 +1,13 @@
 module ForumsHelper
+    def forum_hompage
+        @forum_homepage = Forum.first
+    end
+    
+    def forum_hompage1
+        @forum_hompage1 = Forum.second
+    end
+    
+    def forum_hompage2
+        @forum_hompage2 = Forum.third
+    end
 end
