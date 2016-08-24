72fba5c0db3bdf4745156ed518f1cd2f5c43d9bc
John Galt <ekr990011@gmail.com>
2016-08-24 17:08:35 +0000

    database work with foreign keys

--- db/migrate/20160717173810_create_comments.rb
+++ db/migrate/20160717173810_create_comments.rb
@@ -2,8 +2,8 @@ class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
       t.text :reply
-      t.references :user,  index: true, foreign_key: true
-      t.references :forum,  index: true, foreign_key: true
+      t.references :user,  index: true
+      t.references :forum,  index: true
 
       t.timestamps null: false
     end
